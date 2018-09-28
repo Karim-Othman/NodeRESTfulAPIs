c69616e0973d0303ba82660cf97a960a86770ae0
KarimOthman <karimmohamedothman@gmail.com>
2018-09-28 08:59:30 +0200

    Packages GET and Delete APIs completed.. Documentation updated as well

--- My Work/CiticoBackend/routes/packages.js
+++ My Work/CiticoBackend/routes/packages.js
@@ -43,6 +43,58 @@ router.post('/',async (req,res)=>{
 
 
 
+router.get('/',async(req,res)=>{
+
+    let packages = null;
+    if(req.query.TechName)
+    {
+         packages = await Package.find({TechName:req.query.TechName})
+        .sort({name:1});
+
+        res.send(packages);
+        return;
+    }
+
+    else if(req.query.RelativeTo)
+    {
+         packages = await Package.find({RelativeTo:req.query.RelativeTo})
+        .sort({name:1});
+
+        res.send(packages);
+        return;
+    }
+
+    packages = await Package.find()
+        .sort({name:1});
+
+    res.send(packages);
+
+});
+
+
+
+router.delete('/:TechName',async(req,res)=>{
+
+
+
+    try{
+        let packages = await Package.deleteMany({TechName:req.params.TechName},function (err) {
+            if (err) {res.status(404).send({error:err});
+                        return;
+                        }
+          });
+
+    
+    res.send(packages);
+    }
+
+    catch(ex){
+        res.status(404).send({error:ex});
+    }
+
+});
+
+
 module.exports = router;
 
 async function CreatePackage (body)
